32045ccffcb130d7e062bce74d806c896e782383
Nicholas Westlake <nic@theblinkingproject.com>
2012-01-08 11:06:42 -0600

    don't log so much

--- build/remove.js
+++ build/remove.js
@@ -3,9 +3,7 @@ module.exports = {
   run: function(root, path, settings, doc, callback) {
     var k;
     if (settings["stylus"]["remove_from_attachments"]) {
-      console.log("removing");
       for (k in doc._attachments || {}) {
-        console.log("checking k: " + k);
         if (/\.styl$/.test(k)) {
           delete doc._attachments[k];
         }
--- src/remove.coffee
+++ src/remove.coffee
@@ -2,8 +2,6 @@ module.exports =
   after: "attachments"
   run: (root, path, settings, doc, callback) ->
     if settings["stylus"]["remove_from_attachments"]
-      console.log "removing"
       for k of (doc._attachments or {})
-        console.log "checking k: " + k
         delete doc._attachments[k]  if /\.styl$/.test(k)
     callback null, doc
